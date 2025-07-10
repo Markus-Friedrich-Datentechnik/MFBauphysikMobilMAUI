75e47cb35c144d528f7f07d9194cb33c5dc4a80c
phuongLPsLP <phuong@haufschild.de>
2025-07-10 11:30:33 +0200

    Update 10.7 BoxView

--- MFBauphysikMobilMAUI/Platforms/Android/MainApplication.cs
+++ MFBauphysikMobilMAUI/Platforms/Android/MainApplication.cs
@@ -1,5 +1,10 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.Runtime;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Handlers;
+using Microsoft.Maui.Handlers;
 
 namespace MFBauphysikMobilMAUI
 {
@@ -9,6 +14,16 @@ namespace MFBauphysikMobilMAUI
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
+            /*Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) => {
+                if (view is Entry)
+                {
+                    // Remove underline
+                    handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                    // Change placeholder text color
+                    handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.Red));
+                }
+            });*/
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
